2f759b7e40a56306b2d28a453e379e61e44ecfc1
kennnny123 <67042480+kennnny123@users.noreply.github.com>
2023-05-18 12:12:38 +0300

    Add files via upload

---
+++ Lab4_TEST/test/TestSet.js
@@ -0,0 +1,29 @@
+const assert = require('assert')
+
+class TestSet {
+
+  runOpenLoginPage(driver, vars, By) {
+    it('OpenLoginPage', async function () {
+      await driver.get("http://automationpractice.com/")
+
+      if ((await driver.findElements(By.xpath("//a[contains(text(),\'Sign out\')]"))).length > 0) {
+        await driver.findElement(By.linkText("Sign out")).click()
+      }
+
+      await driver.findElement(By.linkText("Sign in")).click()
+      assert(await driver.getTitle() == "Login - My Store")
+    })
+  }
+
+  runTryAuthoriseUseLoginAndPassword(driver, vars) {
+    it('TryAuthoriseUseLoginAndPassword', async function () {
+      await driver.get("http://automationpractice.com/index.php?controller=authentication&back=my-account")
+      await driver.findElement(By.id("email")).sendKeys("ankl_npuedu@example.com")
+      await driver.findElement(By.id("passwd")).sendKeys("023vw9vMZYrCf9w")
+      await driver.findElement(By.css("#SubmitLogin > span")).click()
+      assert(await driver.findElement(By.xpath("//a/span")).getText() == "Balan VV")
+    })
+  }
+}
+
+module.exports = { TestSet };
